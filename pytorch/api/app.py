725c0097bca68ac3781a99cae66496cfd56b8964
Mateo Cuervo <cuervo.taylor@gmail.com>
2023-10-20 14:23:41 -0500

    refactor dockerfile

--- pytorch/api/app.py
+++ pytorch/api/app.py
@@ -1,12 +1,8 @@
-print(1)
 from flask import Flask
 
-print(2)
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    print(3)
-    return "<p>Hello, World!</p>"
+    return "<p>Hello World!!!</p>"
 
-print(4)
